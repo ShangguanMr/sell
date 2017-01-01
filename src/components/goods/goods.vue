1cd061f922bbbdc2a1e7ae5b1cfa2d342624148d
xuanhewang <811300735@qq.com>
2017-01-01 23:52:10 +0800

    vue

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    李闯大傻
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default{};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
